92ae8dceb0d6bb6b7cd559eefb3594850f404309
nicococo <nico.goernitz@gmail.com>
2025-07-10 23:01:47 +0200

    chore(build): clean up build artifacts and update project metadata

--- dist-build-and-upload.sh
+++ dist-build-and-upload.sh
@@ -1,4 +1,11 @@
+rm -rf build dist *.egg-info
+
 python -m build
+
+twine check --strict dist/*
+pip install -U packaging
+twine check --strict dist/*
+
 twine upload dist/*
 # Note Authenticate: 
 # When prompted, enter __token__ for the username and your PyPI API token for the password.
